3f5145a355fe7d68e6bc65fa3ce51e5d11717173
Dresean  Williams <dreseanwilliams@Dreseans-MBP.attlocal.net>
2018-02-01 20:50:19 -0500

    all work done!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Person = require('./models.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const server = express();
 
@@ -14,7 +14,55 @@ const STATUS_USER_ERROR = 422;
 server.use(bodyParser.json());
 
 // Your API will be built out here.
+server.get('/users', (req, res) => {
+  Person
+  .find()
+  .then( (user) => {
+    res.status(200).json(user)
+  })
+  .catch( error => console.log(error))
+});
 
+server.get('/users/:direction', (req, res) => {
+  const { direction } = req.params;
+  Person.find({})
+  .sort({'firstName': direction})
+  .exec((err, sortedUsers) => {
+    if(err) {
+      res.status(STATUS_SERVER_ERROR).json({'something went wrong': err});
+      return;
+    }
+    res.json(sortedUsers);
+  });
+});
+
+server.get('/user-get-friends/:id', (req, res) => {
+  const { id } = req.params;
+  Person.findById(id)
+  .select('friends')
+  .exec((err, friends) => {
+    if(err) {
+      res.status(STATUS_SERVER_ERROR).json({'something went wrong': err});
+      return;
+    }
+    res.json(friends);
+  })
+});
+
+server.put('/user-update/:id', (req, res) => {
+  const { id } = req.params;
+  const { lastName } = req.body;
+  const { firstName } = req.body;
+  Person
+  .findByIdAndUpdate(id, {firstName, lastName})
+  .exec((err, updatedUser) => {
+    if(err){
+      res.json({error: err});
+      return;
+    }
+    res.status(200).json(updatedUser);
+  })
+});
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect('mongodb://localhost/people', {
   useMongoClient: true
@@ -31,3 +79,4 @@ connect.then(
     console.log('************************\n');
   }
 );
+
